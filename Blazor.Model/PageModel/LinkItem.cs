9156d1c47ed6c3d4e207b2e005a39b51061f0269
C N <1210333872@qq.com>
2022-08-22 21:27:07 +0800

    add loginpage

---
+++ Blazor.Model/PageModel/LinkItem.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using OneOf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Model.PageModel
+{
+    public class LinkItem
+    {
+        public string Key { get; set; }
+
+        public OneOf<string, RenderFragment> Title { get; set; }
+
+        public string Href { get; set; }
+
+        public bool BlankTarget { get; set; }
+    }
+}
---
+++ Blazor.UI/Shared/Login.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Components;
+using AntDesign;
+using Blazor.Model.PageModel;
+
+namespace Project.Pages.User {
+  public partial class Login {
+    private readonly LoginParamsType _model = new LoginParamsType();
+
+    [Inject] public NavigationManager NavigationManager { get; set; }
+         
+
+    [Inject] public MessageService Message { get; set; }
+
+    public void HandleSubmit() {
+      if (_model.UserName == "admin" && _model.Password == "ant.design") {
+        NavigationManager.NavigateTo("/");
+        return;
+      }
+
+      if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
+    } 
+  }
+}
